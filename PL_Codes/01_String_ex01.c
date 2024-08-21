0813107fbf88051a8c2259f1a19842e9f1bd07dd
Jonatha R Costa <92397588+jonathacosta@users.noreply.github.com>
2024-08-21 16:14:06 -0300

    Code

--- PL_Codes/01_String_ex01.c
+++ PL_Codes/01_String_ex01.c
@@ -1,8 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+
+struct Estudante
+{char nome[50];
+int idade;    
+float nota;
+};
 
 int main() {
     struct Estudante aluno;
 
-    strcpy(aluno.nome, "Ana");
+    strcpy(aluno.nome,"Ana");
     aluno.idade = 20;
     aluno.nota = 8.5;
 
@@ -10,4 +18,3 @@ int main() {
     printf("Idade: %d\n", aluno.idade);
     printf("Nota: %.2f\n\n", aluno.nota);
     }
-    
